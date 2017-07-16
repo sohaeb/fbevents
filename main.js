4c8680bc8f7ef1a4eb2285c2d1f588f4bf98bf08
Suhaib Abdulghani <snabieel@gmail.com>
2017-07-16 02:56:30 -0400

    make it better

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 var request = require("request");
 
-request('https://graph.facebook.com/uwindsormsa/events?fields=name,picture&limit=10&access_token=301801280168696|FDzuXZs_Mio_vtBjPvH-fYcBglU', function(error, response, body) {
+let accessToken = 'access_token=301801280168696|FDzuXZs_Mio_vtBjPvH-fYcBglU'
+var param = 'events?fields=name,start_time,cover,end_time,place,description&since=now&' + accessToken;
+var url = 'https://graph.facebook.com'
+
+request(  url + '/uwindsormsa/' + param, function(error, response, body) {
     if (!error && response.statusCode == 200) {
         console.log(JSON.parse(body)); // Show the JSON for the Star Wars Character
     }
